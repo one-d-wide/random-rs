d8fccb4c49bb403212cbd67b40328cb7585b3d08
one-d-wide <one-d-wide@protonmail.com>
2023-05-06 21:39:56 +0000

    Fix release tag in flake

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   outputs = { self, nixpkgs }:
     let
       systems = {
-        "x86_64-linux" = "https://github.com/one-d-wide/random-rs/releases/latest/download/random-rs-linux-x86_64?2021529cc4d4fadcc31cdecce83ecf58281f370c293258d2e1fa476752a0dc39";
-        "aarch64-linux" = "https://github.com/one-d-wide/random-rs/releases/latest/download/random-rs-linux-aarch64?01431934b062e31fb691fbf724fb2bf82675cb6b78f8c94d9ff362202c9f1113";
+        "x86_64-linux" = "https://github.com/one-d-wide/random-rs/releases/download/v0.2.2/random-rs-linux-x86_64?2021529cc4d4fadcc31cdecce83ecf58281f370c293258d2e1fa476752a0dc39";
+        "aarch64-linux" = "https://github.com/one-d-wide/random-rs/releases/download/v0.2.2/random-rs-linux-aarch64?01431934b062e31fb691fbf724fb2bf82675cb6b78f8c94d9ff362202c9f1113";
       };
       lib = nixpkgs.lib;
     in
